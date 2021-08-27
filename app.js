1ae602c573d6511432eb06f3aae524e437bbd78e
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-08-27 10:43:36 -0500

    Added optional initialization (node arg) of ngrok in main app logic

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
-const routes = require('./routes');
+const args = process.argv.slice(2);
 
-require('dotenv').config();
+const routes = require("./routes");
+
+require("dotenv").config();
 
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || "production";
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan(env == "development" ? "dev" : "tiny"));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
+
+// Initialized ngrok if requested
+
+if (args.includes("ngrok")) {
+    const ngrok = require("ngrok");
+    (async function () {
+        try {
+            const url = await ngrok.connect(port);
+            console.log(`Tunnel created at: ${url}`);
+        } catch (err) {
+            console.error("Couldn't initialize tunnel");
+            console.error(err);
+        }
+    })();
+}
