efe18b0c55b83401d8274e5b39f5671bed60b4bd
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-08-27 10:00:34 -0500

    Added morgan for logging

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
+
 const routes = require('./routes');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan(env == "development" ? "dev" : "tiny"));
 
 app.use('/api', routes);
 
 app.listen(port, () => {
-  console.log('Example app listening on port: ', port);
+  console.log(`App listening on port ${port}`);
 });
