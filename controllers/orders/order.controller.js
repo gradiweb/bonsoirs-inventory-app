65693650bfbbf1bfd5706fff39f7fbe2e58a49e9
Daniel Saavedra <jdaniel.saavedra@gradiweb.com>
2021-08-27 16:30:09 -0500

    Changed boilerplate code and added handler function for order-created webhook

--- controllers/orders/order.controller.js
+++ controllers/orders/order.controller.js
@@ -1,33 +1,47 @@
 // Package (All npm package)
-require('dotenv').config();
+require("dotenv").config();
 
 // Requires (Service, Utils, graphql[query, mutations, etc...])
-const OrderService = require('../../services/orders/order.service')
+const { OrderService } = require("../../services/orders/order.service");
 
 // Instances (All class instances)
-const orderServiceInstance = new OrderService()
-
-
-
+const orderServiceInstance = new OrderService();
 
 module.exports = class Order {
-
     shipupOrder = async (req, res) => {
-
-
         if (!req.body) return res.sendStatus(200);
-        
+
+        const { id: orderId } = req.body;
+
+        const emailTokenized = `c8585${orderId}@shipup.io`;
+
+        let data = {
+            order: {
+                id: orderId,
+                note: emailTokenized
+            }
+        };
+
         try {
+            const serverResponse = await orderServiceInstance.updateOrder(orderId, data);
 
-            const { id: orderId } = req.body;
-
-            const emailTokenized = await orderServiceInstance.updateOrder(orderId)
-
-            return res.status(200).send(`Added a note with ${emailTokenized}`);
+            return res.status(200).json({
+                response: `Added a note with ${emailTokenized}`,
+                serverResponse
+            });
         } catch (err) {
             console.error(err);
             return res.sendStatus(500);
         }
-    }
+    };
 
-}
+    handleNewOrder = async (req, res) => {
+        if (!req.body) return res.sendStatus(200);
+
+        const { id: orderId } = req.body;
+
+        console.log(`Received webhook for the creation of order #${orderId}`);
+        console.log("Payload:");
+        console.log(req.body);
+    };
+};
