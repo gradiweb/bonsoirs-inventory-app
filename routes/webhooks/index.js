f2e33a33dce11c25da158ca5356b5c2dc7a37d69
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-09-06 10:52:55 -0500

    Renamed endpoint

--- routes/webhooks/index.js
+++ routes/webhooks/index.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
-const order = require('./orders/order.route');
+const router = require("express").Router();
+const order = require("./orders/order.route");
 
-router.use('/order', order);
+router.use("/orders", order);
 
 module.exports = router;
