f195e2b0684957b8d8a05c81a9d92e7e2b7fdd48
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-09-06 17:51:45 -0500

    Added method for extracting bundles' sub-product handles

--- services/products/product.service.js
+++ services/products/product.service.js
@@ -21,6 +21,35 @@ class ProductService {
         });
         return response;
     }
+
+    async getBundleSubProducts(line_items) {
+        const data = [];
+
+        for (const item of line_items) {
+            const { product_id, quantity } = item;
+            try {
+                const response = await this.getProduct(product_id);
+                const { product } = response;
+
+                if (!product.tags.includes("package")) continue;
+
+                // If the product is a bundle, get inner products' handles from tags
+                const innerProducts = product.tags
+                    .filter(tag => tag.match(/^product-/))
+                    .map(el => el.split("product-")[1]);
+
+                if (innerProducts.length === 0) continue;
+
+                innerProducts.forEach(el => data.push({ handle: el, quantity }));
+            } catch (err) {
+                console.error(err);
+
+                return null;
+            }
+        }
+
+        return data;
+    }
 }
 
 module.exports = { ProductService };
