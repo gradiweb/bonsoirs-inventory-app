fe7b32994bde4a30b1981a7d6d65f5c66f453610
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-09-08 17:33:52 -0500

    Added final part of logic: add variants to order, add 100% discount and commit changes

--- services/products/product.service.js
+++ services/products/product.service.js
@@ -5,7 +5,11 @@ const {
     getProductByHandle,
     getVariantBySKU
 } = require("../../graphql/products/queries/product.query");
+
 const graphQLClient = require("../../graphql");
+
+const { OrderService } = require("../orders/order.service");
+const orderServiceInstance = new OrderService();
 
 const { SHOPIFY_GQL_ENDPOINT: gql_url, SHOPIFY_GQL_BASEID: gql_baseId } = process.env;
 
@@ -73,13 +77,19 @@ class ProductService {
         return data;
     }
 
-    async handleReduceInventories(item) {
+    async handleReduceInventories(item, orderId) {
         // item includes line_item info: subproduct handles, properties and quantity
         const { quantity } = item;
+        let response;
+
+        console.log("Begin order edit");
+        response = await orderServiceInstance.beginOrderEdit(orderId);
+        console.log(response);
+        let order = response.orderEditBegin.calculatedOrder;
 
         for (const handle of item.subproductHandles) {
             // Product info includes variant info (with inventory item and levels), see /graphql/products/queries/product.query.js
-            const response = await this.getProductByHandle(handle);
+            response = await this.getProductByHandle(handle);
             const { productByHandle: productInfo } = response;
 
             if (!productInfo) {
@@ -102,12 +112,37 @@ class ProductService {
 
             const { value: SKU } = prop;
 
-            const variantInfo = productInfo.variants.edges.find(el => el.node.sku == SKU);
+            const variantInfo = productInfo.variants.edges.find(el => el.node.sku == SKU).node;
 
             console.log("Variant info found:");
             console.log("----------------------------------");
             console.log(JSON.stringify(variantInfo, 0, 2));
+
+            // Add variant to order
+            console.log(`Adding variant ${variantInfo.title} to order`);
+            response = await orderServiceInstance.addVariantOrderEdit(
+                order.id,
+                variantInfo.id,
+                quantity
+            );
+            console.log(response);
+            order = response.orderEditAddVariant.calculatedOrder;
+
+            const newLineItem = order.addedLineItems.edges.find(
+                el => el.node.title == productInfo.title
+            ).node;
+
+            console.log("Adding full discount");
+            response = await orderServiceInstance.addFullDiscountOrderEditLineItem(
+                order.id,
+                newLineItem.id
+            );
+            console.log(response);
         }
+
+        console.log("Commit changes made to order");
+        response = await orderServiceInstance.endOrderEdit(order.id);
+        console.log(response);
     }
 }
 
